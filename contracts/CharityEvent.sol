c2d475caa3fc775390d3577f306ae1487aeea2f4
Damien Foo <140382328+DamienFooxx@users.noreply.github.com>
2025-10-27 23:23:46 +0800

    fixed wrong merge on CharityEvent

--- contracts/CharityEvent.sol
+++ contracts/CharityEvent.sol
@@ -294,7 +294,6 @@ contract CharityEvent is Registry {
     /**
      * @dev Transition to a new phase
      */
-    function requestRetry() external;
     function _transitionPhase(EventPhase newPhase) internal {
         EventPhase oldPhase = phase;
         phase = newPhase;
